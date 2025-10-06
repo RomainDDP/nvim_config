c2bf45a968d4c006d531381e674f9e91383537bd
Romain DDP <romain.d77320@gmail.com>
2025-10-06 10:25:16 +0200

    Markdown renderer addition, SUPER+md for toggle

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -24,6 +24,18 @@ vim.g.clipboard = {
   cache_enabled = true,
 }
 
+-- for line break if too long, and only on certain filetype
+vim.api.nvim_create_autocmd({"BufRead","BufNewFile"}, {
+  pattern = {"*.md","*.txt", "*.log"},
+  callback = function()
+    vim.opt_local.wrap = true
+    vim.opt_local.linebreak = true
+    vim.opt_local.breakindent = true
+    vim.opt_local.showbreak = "↪ "
+    vim.opt_local.colorcolumn = "80"
+  end,
+})
+
 
 
 vim.g.mapleader = "M"				-- LEADER key, super important !
